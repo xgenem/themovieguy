d9b4808019103bb23f655f744693e9001125f728
Eugene <eug5ne@gmail.com>
2025-05-16 09:19:34 +0800

    feat: add filtering

--- api/movies.ts
+++ api/movies.ts
@@ -1,8 +1,8 @@
 import API from "@/utils/api";
 
-export const fetchAllMovies = async () => {
+export const fetchAllMovies = async (filter: string) => {
   try {
-    const response = await API.get("trending/all/day");
+    const response = await API.get(`trending/${filter}/day`);
     return response.data;
   } catch (error) {
     console.error("Error fetching movies: ", error);
--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,20 @@ import { fetchAllMovies } from "@/api/movies";
 import MovieCard from "@/components/MovieCard";
 import { FlashList, ListRenderItem } from "@shopify/flash-list";
 import { useQuery } from "@tanstack/react-query";
-import React, { useCallback } from "react";
-import { ActivityIndicator, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { ActivityIndicator, Button, View } from "react-native";
 
 export default function Index() {
+  const [filter, setFilter] = useState("tv");
   const { data: movies = [], isLoading } = useQuery({
-    queryKey: ["movies", "popular"],
-    queryFn: fetchAllMovies,
+    queryKey: ["movies", filter],
+    queryFn: async () => await fetchAllMovies(filter),
     select: (data) => data.results as Movie[],
   });
+
+  const onSelectFilter = (filter: string) => {
+    setFilter(filter);
+  };
 
   const renderItem = useCallback<ListRenderItem<Movie>>(
     ({ item }) => <MovieCard key={item.id} movie={item} />,
@@ -32,8 +37,24 @@ export default function Index() {
       renderItem={renderItem}
       estimatedItemSize={200}
       contentContainerStyle={{ paddingBottom: 100 }}
+      ListHeaderComponent={<Filters onPress={onSelectFilter} />}
       showsVerticalScrollIndicator={false}
       numColumns={2}
     />
   );
 }
+
+const Filters = ({ onPress }: { onPress: (filter: string) => void }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10,
+      }}
+    >
+      <Button title="TV" onPress={() => onPress("tv")} />
+      <Button title="Movies" onPress={() => onPress("movie")} />
+    </View>
+  );
+};
